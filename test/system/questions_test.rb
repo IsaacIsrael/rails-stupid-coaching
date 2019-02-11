f3aae46f765364f1960af0ddfb3c9bd550e68e56
isaac israel <email.iisrael@gmail.com>
2019-02-11 14:00:26 -0200

    add testing , and ask feature

---
+++ test/system/questions_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class QuestionsTest < ApplicationSystemTestCase
+  test 'visiting/ask renders the form' do
+    visit ask_url
+    assert_selector 'label', text: 'Ask your coach anything:'
+  end
+
+  test 'saying Hello yield a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+  test 'saying Hello? yield a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello?'
+    click_on 'Ask'
+
+    assert_text 'Silly question, get dressed and go to work!.'
+  end
+  test 'saying I am going to work yield a good response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'I am going to work'
+    click_on 'Ask'
+
+    assert_text 'Great!'
+  end
+  test 'saying nothing yield a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: ''
+    click_on 'Ask'
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+end
