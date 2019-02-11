57a66225085711d463fc43b11470cd565344f92b
isaac israel <email.iisrael@gmail.com>
2019-02-11 12:34:02 -0200

    Initialize the ask feature

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,32 @@
+# frozen_string_literal: true
+
+# Question Controller
 class QuestionsController < ApplicationController
   def ask
   end
 
   def answer
+    @question = params[:question]
+    @awnser = stupid_awnser(@question)
+  end
+
+  private
+
+  def affirmative_phrases_awnser
+    "I don't care, get dressed and go to work!"
+  end
+
+  def aquestion_phrases_awnser
+    'Silly question, get dressed and go to work!.'
+  end
+
+  def good_awnser
+    'Great!'
+  end
+
+  def stupid_awnser(question)
+    return good_awnser if question == 'I am going to work'
+
+    question.last == '?' ? aquestion_phrases_awnser : affirmative_phrases_awnser
   end
 end
